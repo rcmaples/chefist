ce79a744c5da7dc51f4ce176ea710a439b0ef3bf
RC Maples <rcmaples@users.noreply.github.com>
2018-07-07 09:54:57 -0400

    Successful API Call and results from form submittal. \o/

--- js/app.js
+++ js/app.js
@@ -1,69 +1,69 @@
 'use strict';
- 
-/* 
+
+/*
  * Chefist app.js
  * © 2018 - RC Maples
- * 
+ *
  * -----------------------------------------------
- * 
+ *
  * API Base URL:
  * https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/
- * 
+ *
  * API Endpoints:
- * 
+ *
  * Initial Search:
- * findByIngredients? - Params: 
+ * findByIngredients? - Params:
  *                  fillIngredients=false   -- Add information about the used and missing ingredients in each recipe.
  *                  &ingredients=chicken,rice noodles,egg -- A comma-separated list of ingredients that the recipes should contain.
  *                  &limitLicense=true -- Whether to only show recipes with an attribution license.
  *                  &number=10 -- The maximal number of recipes to return (default = 5).
  *                  &ranking=1 -- Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
  * (https://market.mashape.com/spoonacular/recipe-food-nutrition#search-recipes-by-ingredients)
- * 
- * 
+ *
+ *
  * Display Summary:
  * ${recipe-id}/summary
  * (https://market.mashape.com/spoonacular/recipe-food-nutrition#summarize-recipe)
- * 
- * 
+ *
+ *
  * Display Recipe:
  * ${recipe-id}/information? - Params:
- *                  includeNutrition=true -- Include nutrition data to the recipe information. Nutrition data is per serving. 
+ *                  includeNutrition=true -- Include nutrition data to the recipe information. Nutrition data is per serving.
  *                                           If you want the nutrition data for the entire recipe, just multiply by the number of servings.
  * https://market.mashape.com/spoonacular/recipe-food-nutrition#get-recipe-information
- * 
+ *
  * -----------------------------------------------
- * 
+ *
  * Application Logic:
- * 
+ *
  * First, we will grab the input data from the form
  * and store it in a string variable.
- * 
+ *
  * With the String Variable in hand, we need to pass
  * it to the findByIngredients endpoint.
- * 
+ *
  * With the data we get back from the first API call,
- * we then need to pass each recipe ID returned into 
- * the summary endpoint 1x1. 
- * 
- * Once we have the summary of each recipe, we need 
- * jQuery to create a summary card item and display 
+ * we then need to pass each recipe ID returned into
+ * the summary endpoint 1x1.
+ *
+ * Once we have the summary of each recipe, we need
+ * jQuery to create a summary card item and display
  * it. We'll be using Desandro's Masonry to display
  * the results.
  * https://masonry.desandro.com/ (MIT License)
- * 
- * The results will have infinite scroll load using 
- * Metafizzy's Infinite Scroll 
+ *
+ * The results will have infinite scroll load using
+ * Metafizzy's Infinite Scroll
  * https://infinite-scroll.com/ (GNU GPL 3)
- * 
+ *
  * User should be able to return to search form from
  * search results to start over.
- * 
- * When the user clicks on a summary card, we will 
+ *
+ * When the user clicks on a summary card, we will
  * call the information end point with the recipe's
- * ID number from the summary card. This API call 
+ * ID number from the summary card. This API call
  * will likely take a bit of time to run.
- * 
+ *
  * User should be able to return to Search Results
  * from their selected recipe.
  –––––––––––––––––––––––––––––––––––––––––––––––––– */
@@ -72,20 +72,24 @@
 
 /* API Section :)
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+// TODO: remove recipeId from global scope; will use for testing and to prevent js erro on line 81/82 for now.
+// TODO: CLEAN UP GLOBAL VARS IF NOT NEEDED!
+let recipeId = ""; //
 let SEARCH_QUERY = `chicken breast,split peas,mangos,olive oil,butter,green beans,corn on the cob`; /* for initial test and QA. */
 // let SEARCH_QUERY = ``;
 const SPOON_BASE_URL = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/`;
 const INGREDIENT_SEARCH_STRING = `findByIngredients?fillIngredients=false&ingredients=${SEARCH_QUERY}&limitLicense=true&number=10&ranking=1`;
-const GET_RECIPE_STRING = `${recipe-id}/information?includeNutrition=true`;
-const GET_SUMMARY_RESULTS = `${recipe-id}/summary`;
+const GET_RECIPE_STRING = `${recipeId}/information?includeNutrition=true`;
+const GET_SUMMARY_RESULTS = `${recipeId}/summary`;
 
-/* callApi takes a base url, query string, and 
- * callback upon successful api request, will 
+/* callApi takes a base url, query string, and
+ * callback upon successful api request, will
  * perform callback. upon error, will run apiError
  * function to log errors to the console. Will call
  * setHeader before the api request for auth etc
  –––––––––––––––––––––––––––––––––––––––––––––––––– */
-function callApi(baseUrl, query, callback) { // Generalized for portability. On Success run callback function. 
+function callApi(baseUrl, query, callback) { // Generalized for portability. On Success run callback function.
     $.ajax({
         url: `${baseUrl}${query}`,
         type: 'GET',
@@ -139,6 +143,8 @@ function onFormSubmit(){
         }
         ingredientString = ingredientString.slice(0,ingredientString.length-1); // remove that comma
         console.log(ingredientString); // return!
+        SEARCH_QUERY = ingredientString;
+        callApi(SPOON_BASE_URL, INGREDIENT_SEARCH_STRING, apiTest);
     })
 
 }
@@ -174,7 +180,7 @@ function getRidOfSimilar (myStr) {
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 function jinkies(){
     increaseFormFields(3); // We start with 2 by default, so when the app starts, we pre-set 3 into the function.
-    onFormSubmit(); 
+    onFormSubmit();
     console.log('Jinkies!');
 };
 $(jinkies);
